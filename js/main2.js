d3710153634fb0fda432337cb8f54aa33c932f95
kschlind <karaschlindwein@gmail.com>
2015-07-28 21:12:56 -0400

    Switched else and else if.

--- js/main2.js
+++ js/main2.js
@@ -3,15 +3,14 @@ $(document).ready(function () {
 	$('#primary-nav li').click (function(e) {
 		//prevent jumping around on page
 		e.preventDefault();
-
-		$(this).addClass('active');
-		$(this).siblings().removeClass('active');
 		
 
 	//handles slide down
 
 		//if slide-down is not open
 		if (!$('#slide-down').hasClass('open')) {
+			//alert('if');
+
 			$('#slide-down').addClass('open');
 			$('#slide-down').css('display','block');
 			var section = $(this).children('a').attr('href');
@@ -19,10 +18,27 @@ $(document).ready(function () {
 			console.log(section);
 			$(section).siblings().css('display','none');
 
+		$(this).addClass('active');
+		$(this).siblings().removeClass('active');
 
 		//else if slide-down is open and link has active class
 
 		} else if ($('#slide-down').hasClass('open') && $(this).hasClass('active')) {
+			//alert('else if');
+
+			
+			$(this).removeClass('active');
+			$('#slide-down').removeClass('open');
+			$('#slide-down').css('display','none');
+			$('#slide-down').children().hide();
+
+		//else (clicked on nonactive link while slide-down is open)
+			
+		} else {
+			//alert('else');
+
+			$(this).addClass('active');
+			$(this).siblings().removeClass('active');
 
 			$('#slide-down').css('display','block');
 			var section = $(this).children('a').attr('href');
@@ -30,16 +46,6 @@ $(document).ready(function () {
 			console.log(section);
 			$(section).siblings().css('display','none');
 
-			
-
-		//else (clicked on nonactive link while slide-down is open)
-			
-		} else {
-
-			$(this).removeClass('active');
-			$('#slide-down').removeClass('open');
-			$('#slide-down').css('display','none');
-			$('#slide-down').children().css('display','none');
 
 
 		}
